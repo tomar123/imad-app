0a4209805fa067527609ad873dbc8c31c4b933b3
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-03 23:36:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));});
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    
+});
 
 app.get('/article-two',function(req,res){
    res.send('Article two requested and will be served here'); 
@@ -19,13 +21,6 @@ app.get('/article-two',function(req,res){
 app.get('/article-three',function(req,res){
    res.send('Article three requested and will be served here'); 
 });
-
-
-
-
-
-
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
