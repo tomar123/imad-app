aa72085e2c53e9820412b8992fa47c4c3f655161
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-04 14:57:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
-    res.send(createTemplate(article[articleName]));
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
