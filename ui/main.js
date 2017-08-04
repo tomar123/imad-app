35f2e65f323c24300b3dc99540757c58f551b001
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-04 18:50:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,9 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innerHTML='New Value';
+
+var img=document.getElementById('madi');
+
+madi.onclick=function(){
+  madi.style.marginright='150px';  
+};
