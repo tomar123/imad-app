90309fc7040aa1e9aab85224e241c78e9325dd77
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-05 16:12:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,6 +38,25 @@ button.onclick=function(){
 };
 
 
+//submit button
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function() {
+  //Make a request to the server and send the name
+  
+  //Capture a list of names and render it as a list
+  var names = ['name1', 'name2', 'name3', 'name4'];
+  var list = '';
+  for(var i=0; i<names.length; i++){
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
+
+
 element.innerHTML='New Value';
 
 var img=document.getElementById('madi');
@@ -56,26 +75,6 @@ img.onclick=function(){
 
 
 
-
-//submit button
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-
-submit.onclick = function() {
-  //Make a request to the server and send the name
-  
-  //Capture a list of names and render it as a list
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list = '';
-  for(var i=0; i<names.length; i++){
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
-  
-    
-};
 
 
 
