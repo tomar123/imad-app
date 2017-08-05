eed6ff9c3e629cb2d02217843632aec2003b1e8e
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-05 14:07:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,24 @@ img.onclick=function(){
   var interval = setInterval(moveLeft,50);
   //img.style.marginRight='150px';  
 };
+
+
+
+
+var button=document.getElementById('counter');
+var counter = 0;
+
+button.onclick=function(){
+    
+    //Make a request to the counter endpoint
+    
+    //Capture the response and store it in a variable
+    
+    //Render the variable in the correct span
+  
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+};
+
