4330f28bb429ea5c6ed08cf694d9e26bf30bfd3e
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-05 16:30:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,13 +108,13 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req,res){
+app.get('/submit-name/:name', function(req,res) {
    //Get the name from the request
    var name = req.params.name;
    
    names.push(name);
    //JSON: Javascript Object Notation
-   req.send(JSON.stringify(names));
+   res.send(JSON.stringify(names));
 });
 
 
