9c4a0c64affa9c25be799274aa4933a4f531efa9
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-12 22:05:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ app.get('/articles/:articleName',function(req,res) {
     //articles[articleName] =={} content object for article-one
     
     //SELECT * FROM article WHERE title = 'article-one'
-    pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName + "'", function (err, result) {
+    pool.query("SELECT * FROM article WHERE title = $1" ,[req.params.articleName], function (err, result) {
         if(err) {
             res.status(500).send(err.toString());
         } else {
