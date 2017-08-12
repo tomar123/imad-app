e453629832e3b731262082098437998e4a381484
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-12 12:35:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config ={
+    user: 'deepaktomar2031',
+    database: 'deepaktomar2031',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -106,6 +115,22 @@ app.get('/submit-name', function(req,res) { // /submit-name?name=something
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config);
+app.get('/test-db', function(req,res){
+    //make a select request
+    //return a request with results
+    pool.query('SELECT * FROM test', function (err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send(JSON.stringfy(result));
+       }
+    });
+    
+});
+
+
 
 app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
