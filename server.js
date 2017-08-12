a927650abcdebb2a759f30b110d720cd39b96519
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-12 15:09:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.get('/articles/:articleName',function(req,res) {
         if(err) {
             res.status(500).send(err.toString());
         } else {
-            if (result.raws.length ===0) {
+            if (result.rows.length === 0) {
                 res.status(404).send('Article Not Found');
             } else {
                 var articleData = result.rows[0];
