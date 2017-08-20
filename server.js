5634eebbe4da443464a4bd34c58e02d1a9a0ebcc
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-21 01:06:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ app.post('/login', function(res, req)
 {
    var username = req.body.username;
    var password = req.body.password;
-   pool.query('SELECT * FROM "user" username = $1', [username], function (err, result)
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result)
    {
        if (err) 
        {
