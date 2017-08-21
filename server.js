3da0a5adae487933eff8b01cf69ebbd0679bea85
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-21 20:43:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ app.post('/create-user', function(req, res){
 
 
 
-app.post('/login', function(res, req)
+app.post('/login', function(req, res)
 {
    var username = req.body.username;
    var password = req.body.password;
