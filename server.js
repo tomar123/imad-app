3dc711812509a8e1e9fd9c6abaa53559958ef89c
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-21 20:35:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config ={
     user: 'deepaktomar2031',
