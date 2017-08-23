e13b091a9ba443ace742588c2f0379569526f73e
tomar123 <30664265+tomar123@users.noreply.github.com>
2017-08-23 21:04:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,7 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 
+/*
 //counter code
 var button=document.getElementById('counter');
 //var counter = 0;
@@ -36,8 +37,9 @@ button.onclick=function(){
     //span.innerHTML = counter.toString();
     
 };
+*/
 
-
+/*
 //submit button
 
 var submit = document.getElementById('submit_btn');
@@ -73,7 +75,48 @@ submit.onclick = function() {
     request.open('GET', 'http://deepaktomar2031.imad.hasura-app.io/submit-name?name=' +name, true);
     request.send(null);
 };
+*/
 
+//submit username/password to login
+
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function() {
+    
+        //Create a request object
+        var request = new XMLHttpRequest();
+    
+        //Capture the response and store it in a variable
+        request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take Some Action
+            if(request.status ===200)
+            {
+                //Capture a list of names and render it as a list
+                console.log('user logged in');
+                alert('logged in successfully');
+            }
+            else if (request.status ===403)
+            {
+                alert('username/password is incorrect');
+            }
+            else if(request.status ===500)
+            {
+                alert('something went wrong on the server');
+            }
+        }
+        //Not Done Yet
+    };
+    
+    //Make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://deepaktomar2031.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
 
 element.innerHTML='New Value';
 
